8613ff08f7ec44880d51d4ae531c033349f3b838
Sunny Chanthavong <sudochanth@gmail.com>
2019-08-22 16:41:08 -0600

    deploying scripts

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 7000;
+const path = require("path")
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 
 const Employee = require('./routes/EmployeeRoutes');
 
 app.use(bodyParser.json());
 
 mongoose
-  .connect('mongodb://localhost:27017/employees', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/employees', {useNewUrlParser: true})
   .then( () => console.log(`connected to mongodb on port ${PORT}!!`))
   .catch(err => console.log(err))
 
 app.use("/employees", Employee);
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
