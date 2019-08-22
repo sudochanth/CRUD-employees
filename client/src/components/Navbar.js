f76bdd2bad5963cca0c2d8f0ea9e9eb6e7c84b64
Sunny Chanthavong <sudochanth@gmail.com>
2019-08-22 15:30:23 -0600

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import '../css/navbar.scss';
 const Navbar = ({ visibleToggle }) => {
   return (
     <div className="navbar">
-      <a href='#'>View Employees</a>
+      <a href='#employeeList'>View Employees</a>
       <a href='#employeeForm' onClick={visibleToggle}>Create Employee</a>
     </div>
   );
